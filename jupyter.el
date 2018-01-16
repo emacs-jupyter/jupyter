64c9079ea023216de10884bb1c2f516c987ddd9e
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2018-01-16 11:07:50 -0600

    Add `jupyter.el`

---
+++ jupyter.el
@@ -0,0 +1,44 @@
+;;; jupyter.el --- Jupyter -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Nathaniel Nicandro
+
+;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
+;; Created: 11 Jan 2018
+;; Version: 0.0.1
+;; X-URL: https://github.com/dzop/jupyter
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defgroup jupyter nil
+  "Jupyter"
+  :group 'processes)
+
+(require 'jupyter-base)
+(require 'jupyter-connection)
+(require 'jupyter-client)
+(require 'jupyter-kernelspec)
+(require 'jupyter-repl-client)
+(require 'jupyter-kernel-manager)
+
+(provide 'jupyter)
+
+;;; jupyter.el ends here
