b5d9b98f17c8bc1a955391f7a7dcc0bab411f25b
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2018-01-17 20:00:44 -0600

    Add header to `jupyter-kernelspec`

--- jupyter-kernelspec.el
+++ jupyter-kernelspec.el
@@ -1,3 +1,36 @@
+;;; jupyter-kernelspec.el --- Jupyter kernelspecs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Nathaniel Nicandro
+
+;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
+;; Created: 17 Jan 2018
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defgroup jupyter-kernelspec nil
+  "Jupyter kernelspecs"
+  :group 'jupyter)
+
 (require 'json)
 
 (defvar jupyter--kernelspecs nil
@@ -67,3 +100,5 @@ Optional argument FORCE-NEW has the same meaning as in
      (jupyter-available-kernelspecs force-new))))
 
 (provide 'jupyter-kernelspec)
+
+;;; jupyter-kernelspec.el ends here
