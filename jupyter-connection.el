8ddaa2e469d3018b6355ced403f8a55e4d1b5c85
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2018-01-22 18:36:16 -0600

    Add header to `jupyter-connection.el`

--- jupyter-connection.el
+++ jupyter-connection.el
@@ -1,3 +1,36 @@
+;;; jupyter-connection.el --- Some form of connection to a Jupyter kernel -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Nathaniel Nicandro
+
+;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
+;; Created: 20 Jan 2018
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defgroup jupyter-connection nil
+  "Some form of connection to a Jupyter kernel"
+  :group 'jupyter)
+
 (require 'jupyter-base)
 (require 'eieio-base)
 
@@ -98,3 +131,5 @@ created."
     (jupyter-connect-endpoint sock-type endpoint identity)))
 
 (provide 'jupyter-connection)
+
+;;; jupyter-connection.el ends here
