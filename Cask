3c05232a90416d94f8e79466553f8c5f29714a62
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2018-11-14 18:18:36 -0600

    Add Cask file

---
+++ Cask
@@ -0,0 +1,10 @@
+(source gnu)
+(source melpa)
+
+(package-file "jupyter.el")
+
+(files "*.el" "js" "jupyter.png" "widget.html")
+
+(development
+ (depends-on "ert-runner")
+ (depends-on "julia-mode"))
--- jupyter.el
+++ jupyter.el
@@ -5,7 +5,8 @@
 ;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
 ;; Created: 11 Jan 2018
 ;; Version: 0.0.1
-;; X-URL: https://github.com/dzop/jupyter
+;; Package-Requires: ((emacs "26") (zmq "0.9.0") (cl-lib "0.5") (simple-httpd "1.5.0") (websocket "1.9"))
+;; URL: https://github.com/dzop/emacs-jupyter
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -24,7 +25,7 @@
 
 ;;; Commentary:
 
-;;
+;; An interface for communicating with Jupyter kernels.
 
 ;;; Code:
 
