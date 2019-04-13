81c8a838f88944b5a3246fb66e3562c88631cd34
Jack Kamm <jackkamm@gmail.com>
2019-04-13 09:59:53 -0700

    Remove buggy R html parsing

--- jupyter-R.el
+++ jupyter-R.el
@@ -35,76 +35,16 @@
 (cl-defmethod jupyter-org-result ((_mime (eql :text/html)) params data
                                   &context (jupyter-lang R)
                                   &optional metadata)
-  "Parse and convert DATA of type 'text/html' into a prettier form.
-If _METADATA has ':isolated' tag (e.g. DT::datatable()), save it to a file
-and open in a browser. Otherwise, if libxml is available and DATA
-is of type <table>, <ol>, <ul>, or <dl>, try to render it nicely."
+  "If html DATA is an iframe, save it to a separate file and open in browser.
+Otherwise, parse it as normal."
   (if (plist-get metadata :isolated)
-      ;; handle html with "isolated" property
       (let ((file (or (alist-get :file params)
                       (jupyter-org-image-file-name data ".html"))))
         (with-temp-file file
           (insert data))
         (browse-url-of-file file)
         (jupyter-org-file-link file))
-    ;; handle html without "isolated" property
-    (if (functionp 'libxml-parse-html-region)
-        ;; parse with libxml
-        (with-temp-buffer
-          (insert data)
-          (let* ((parsed-html (libxml-parse-html-region
-                               (point-min) (point-max)))
-                 (inner-element
-                  (jupyter-org-result--R-html-inner-element parsed-html))
-                 (inner-element-type (car inner-element)))
-            (cl-case (car inner-element)
-              (table
-               (jupyter-org-result--R-html-table parsed-html))
-              ((ol dl ul)
-               (jupyter-org-result--R-html-list parsed-html))
-              (t (cl-call-next-method)))))
-      ;; libxml not available, call next method
-      (cl-call-next-method))))
-
-(defun jupyter-org-result--R-html-table (parsed-html)
-  "Convert PARSED-HTML into an org-element table."
-  (with-temp-buffer
-    (shr-insert-document parsed-html)
-    (org-table-convert-region (point-min) (point-max) nil)
-    (org-table-insert-hline)
-    (let* ((tbl (org-table-to-lisp))
-           (head (jupyter-org-result--R-html-table-fix-header (car tbl)))
-           (body (cdr tbl)))
-      (jupyter-org-scalar `(,head . ,body)))))
-
-(defun jupyter-org-result--R-html-table-fix-header (header)
-  "Shifts column names to the right if last element of HEADER is empty string.
-This fixes the header column alignment when the table has row names."
-  (if (equal "" (car (last header)))
-      `("" . ,(butlast header))
-    header))
-
-(defun jupyter-org-result--R-html-list (parsed-html)
-  "Renders list-like PARSED-HTML.
-Since these can have arbitrary nested structure, we don't try to parse it into
-an org-element; instead we just render the html.  Note this is still a bit
-uglier than ':display plain', which should be preferred for these outputs."
-  (with-temp-buffer
-    (shr-insert-document parsed-html)
-    (buffer-string)))
-
-(defun jupyter-org-result--R-html-inner-element (parsed-html)
-  "If PARSED-HTML consists of a body with 1 element, return that element.
-Otherwise, return nil."
-  (let ((html-subnodes (cdr (cdr parsed-html))))
-    ;; check there is 1 html-subnode and it is the body
-    (when (eq 1 (length html-subnodes))
-      (let ((body (car html-subnodes)))
-        (when (eq (car body) 'body)
-          ;; check the body has 1 subnode and return it
-          (let ((body-subnodes (cdr (cdr body))))
-            (when (eq 1 (length body-subnodes))
-              (car body-subnodes))))))))
+    (cl-call-next-method)))
 
 (provide 'jupyter-R)
 
