1546a72f570c7d3a6376d211d27002b2b11ddb40
conao3 <conao3@gmail.com>
2020-03-10 22:16:04 +0900

    clean header

--- jupyter-server.el
+++ jupyter-server.el
@@ -4,7 +4,6 @@
 
 ;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
 ;; Created: 02 Apr 2019
-;; Version: 0.8.0
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
