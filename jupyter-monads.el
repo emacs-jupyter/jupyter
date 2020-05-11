207580968aef8eb2d53e284a52157cd6be15992d
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2020-05-11 13:17:48 -0500

    Add file header to `jupyter-monads.el`

--- jupyter-monads.el
+++ jupyter-monads.el
@@ -1,3 +1,35 @@
+;;; jupyter-monads.el --- Monadic Jupyter I/O -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Nathaniel Nicandro
+
+;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
+;; Created: 11 May 2020
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defgroup jupyter-monads nil
+  "Monadic Jupyter I/O"
+  :group 'jupyter)
+
 (require 'thunk)
 
 (defun jupyter-return (value)
@@ -232,3 +264,8 @@ See `jupyter-io' for more information on IO actions."
         (jupyter-send io 'message ch type content id)
         (jupyter-send io 'add-handler handler)
         req))))
+
+(provide 'jupyter-monads)
+
+;;; jupyter-monads.el ends here
+
