67b7172da075f73f8c35375faac74761445d289f
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2024-10-09 22:58:27 -0500

    Bump minimum supported Emacs version
    * jupyter.el: Do it.

--- jupyter.el
+++ jupyter.el
@@ -5,7 +5,7 @@
 ;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
 ;; Created: 11 Jan 2018
 ;; Version: 1.0
-;; Package-Requires: ((emacs "26") (cl-lib "0.5") (org "9.1.6") (zmq "0.10.10") (simple-httpd "1.5.0") (websocket "1.9"))
+;; Package-Requires: ((emacs "27") (cl-lib "0.5") (org "9.1.6") (zmq "0.10.10") (simple-httpd "1.5.0") (websocket "1.9"))
 ;; URL: https://github.com/emacs-jupyter/jupyter
 
 ;; This program is free software; you can redistribute it and/or
